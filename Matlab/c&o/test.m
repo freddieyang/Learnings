d09e5da7051adbc561ff8451181011395a252601
freddieyang <yf970113@outlook.com>
2019-11-21 14:01:19 +0800

    finish the learning process

---
+++ Matlab/c&o/test.m
@@ -0,0 +1,12 @@
+classdef test
+    properties
+        Age
+        Height
+    end
+    methods
+        function t = test(Age,Height)
+            t.Age = Age;
+            t.Height = Height;
+        end
+    end
+end
